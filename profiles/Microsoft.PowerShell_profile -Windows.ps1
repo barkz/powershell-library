78d2e5797de312e354e995dca02110b2bb8d64d7
barkz <barkz@purestorage.com>
2023-04-20 10:21:52 -0700

    Updated Windows PS profile.

--- profiles/Microsoft.PowerShell_profile -Windows.ps1
+++ profiles/Microsoft.PowerShell_profile -Windows.ps1
@@ -16,6 +16,7 @@ Write-Host '         \++++++\'
 Import-Module -Name PureStoragePowerShellSDK2
 Import-Module -Name PureStoragePowerShellSDK
 Import-Module -Name PureStoragePowerShellToolkit
+Import-Module -Name "\\Mac\Home\Desktop\github-purestorage\powershell-toolkit-3\ImportAll.ps1"
 
 # Disaply Pure Storage module information
 Get-Module -Name *Pure* | Select-Object Name, Version, ModuleType | Format-Table -HideTableHeaders
